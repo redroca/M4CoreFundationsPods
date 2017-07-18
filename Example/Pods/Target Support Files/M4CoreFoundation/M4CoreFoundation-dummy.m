b9515b37dacfc72f15fce8f6361443e5d3c8cd61
Hades <270452746@qq.com>
2017-07-18 14:41:21 +0800

    delete

--- Example/Pods/Target Support Files/M4CoreFoundation/M4CoreFoundation-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_M4CoreFoundation : NSObject
-@end
-@implementation PodsDummy_M4CoreFoundation
-@end
--- Example/Pods/Target Support Files/Pods-M4CoreFoundation_Tests/Pods-M4CoreFoundation_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_M4CoreFoundation_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_M4CoreFoundation_Tests
-@end
